2cc7a293ef41664fcf27ed6b387c755e8f982a03
Maxim Moskalets <maxmosk@list.ru>
2025-05-01 00:21:58 +0300

    refactor: split hyprland from main part
    Change-Id: Id3314f51cd4cd00ca0d5487d318edbb5ea552017

---
+++ config/desktop/hyprland/default.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+  services.xserver.enable = true;
+  programs.hyprland = {
+    enable = true;
+  };
+  programs.hyprlock.enable = true;
+  programs.waybar.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    kitty
+    wofi
+    brightnessctl
+    playerctl
+  ];
+  fonts.packages = with pkgs; [
+    font-awesome
+  ];
+}
