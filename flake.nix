c619d57075fb1fc41616d4f4a41da86c6fc11d45
Maxim Moskalets <maxmosk@list.ru>
2025-05-02 12:47:54 +0300

    feat: add home-manager to flakes
    Change-Id: Ibf9028105cd260585b51cc73b7b9763eca0b7f9c

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.moskalets-nb = nixpkgs.lib.nixosSystem {
       modules = [ ./configuration.nix ];
     };
