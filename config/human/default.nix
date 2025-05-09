d14b123c5974586821ef70c9f51c9cf1cb6fd6ae
Maxim Moskalets <maxmosk@list.ru>
2025-05-09 17:28:04 +0300

    feat: use chrome as default browser
    Change-Id: Ia81fa8dae421e3b1b688e1041a6b4a960b4b7980

--- config/human/default.nix
+++ config/human/default.nix
@@ -23,4 +23,15 @@
       ];
     };
   };
+
+  xdg.mime = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "google-chrome.desktop";
+      "x-scheme-handler/http" = "google-chrome.desktop";
+      "x-scheme-handler/https" = "google-chrome.desktop";
+      "x-scheme-handler/about" = "google-chrome.desktop";
+      "x-scheme-handler/unknown" = "google-chrome.desktop";
+    };
+  };
 }
